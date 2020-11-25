bb468986409238dfec6c48a1d9bc3d14f27379af
Liubov Kholmogorova <holmarst@gmail.com>
2020-11-26 01:35:40 +0300

    add css popup_opened

--- pages/script.js
+++ pages/script.js
@@ -3,4 +3,6 @@ let popup = document.querySelector('.popup');
 let add = document.querySelector('.profile__add-button');
 let close = document.querySelector('.popup__close');
 let save = document.querySelector('.popup__button');
+let name = document.querySelector('.popup__input_type_name');
+let about = document.querySelector('.popup__input_type_about');
 
