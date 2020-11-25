86507f1d9f825980f236f1e80afe5a3f0a477308
Liubov Kholmogorova <holmarst@gmail.com>
2020-11-26 00:49:15 +0300

    create part of let names

---
+++ pages/script.js
@@ -0,0 +1,6 @@
+let edit = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let add = document.querySelector('.profile__add-button');
+let close = document.querySelector('.popup__close');
+let save = document.querySelector('.popup__button');
+
