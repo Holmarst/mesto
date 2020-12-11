20c2012347833da166a94c8edb98d0ee529a9998
Liubov Kholmogorova <holmarst@gmail.com>
2020-12-11 14:26:31 +0300

    cards: добавление карточки

--- script/script.js
+++ script/script.js
@@ -1,58 +1,3 @@
-const editButton = document.querySelector('.profile__edit-button');
-const profileTitle = document.querySelector('.profile__title');
-const profileSubtitle = document.querySelector('.profile__subtitle');
-const form = document.querySelector('.popup__container');
-const nameInput = document.querySelector('.popup__input_type_name');
-const aboutInput = document.querySelector('.popup__input_type_about');
-const addButton = document.querySelector('.profile__add-button');
-const closeAllButtons = document.querySelectorAll('.popup__close');
-const popupAddCard = document.querySelector('.popup_act_add-card');
-const popupEditProfile = document.querySelector('.popup_act_edit-profile');  
- 
-
-// popup / open /close
-
-function openPopup(typeofPopup) {
-  typeofPopup.classList.add('popup_opened');
-  nameInput.value = profileTitle.textContent;
-  aboutInput.value = profileSubtitle.textContent;
-}
-
-closeAllButtons.forEach(function (button) {
-  button.addEventListener('click', function (evt) {
-    evt.target.closest('.popup_opened').classList.remove('popup_opened');
-  });
-}); 
-
-addButton.addEventListener('click', function () {
-  openPopup(popupAddCard);
-});
-
-editButton.addEventListener('click', function () {
-  openPopup(popupEditProfile);
-});
-
-
-// popap / edit profile
-
-function formSubmitHandler (evt) {
-  evt.preventDefault(); 
-
-  profileTitle.textContent = nameInput.value;
-  profileSubtitle.textContent = aboutInput.value;
-
-  if (nameInput.value === '') {
-    profileTitle.textContent = "Имя";
-  }
-  if (aboutInput.value === '') {
-    profileSubtitle.textContent = "Пока нет описания";
-  }
-  closePopup();
-}
-
-form.addEventListener('submit', formSubmitHandler);
-
-
 // 6 cards
 
 const initialCards = [
@@ -82,26 +27,101 @@ const initialCards = [
   }
 ];
 
-const cardContainer = document.querySelector('.elements__container');
 
+const profileTitle = document.querySelector('.profile__title');
+const profileSubtitle = document.querySelector('.profile__subtitle');
+const nameInput = document.querySelector('.popup__input_type_name');
+const aboutInput = document.querySelector('.popup__input_type_about');
+const placeInput = document.querySelector('.popup__input_input_name');
+const linkInput = document.querySelector('.popup__input_input_link');
+
+const addButton = document.querySelector('.profile__add-button');
+const editButton = document.querySelector('.profile__edit-button');
+const closeAllButtons = document.querySelectorAll('.popup__close');
+
+const popupAddCard = document.querySelector('.popup_act_add-card');
+const popupEditProfile = document.querySelector('.popup_act_edit-profile');  
+
+const formEditProfile = document.querySelector('.popup__container');
+const formAddCard = document.querySelector('.popup__container_act_add-card');
+
+const cardContainer = document.querySelector('.elements__container');
+const cardTemplate = document.querySelector('.card-template');
+
+// popup / open /close
+
+function openPopupEditProfile() {
+  popupEditProfile.classList.add('popup_opened');
+  nameInput.value = profileTitle.textContent;
+  aboutInput.value = profileSubtitle.textContent;
+}
+
+function openPopupAddCard() {
+  popupAddCard.classList.add('popup_opened');
+}
+
+function closePopup(evt) {
+  evt.target.closest('.popup_opened').classList.remove('popup_opened');
+}
+
+closeAllButtons.forEach(function (button) {
+  button.addEventListener('click', closePopup);
+}); 
+
+editButton.addEventListener('click', function () {
+  openPopupEditProfile();
+});
+
+addButton.addEventListener('click', function () {
+  openPopupAddCard();
+});
+
+// popap / edit profile
+
+function formSubmitHandler (evt) {
+  evt.preventDefault(); 
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = aboutInput.value;
+
+  if (nameInput.value === '') {
+    profileTitle.textContent = "Имя";
+  }
+  if (aboutInput.value === '') {
+    profileSubtitle.textContent = "Пока нет описания";
+  }
+  closePopup(evt);
+}
+
+// render cards
 
 function renderCards() {
-  const listCards = initialCards.map(addCard);
+  const listCards = initialCards.map(composeItem);
   cardContainer.append(...listCards);
 }
 
-function addCard(card) {
-  const cardTemplate = document.getElementById('card-template').content;
-  const cardElement = cardTemplate.cloneNode(true);
-
-  cardElement.querySelector('.element__title').textContent = card.name;
-  cardElement.querySelector('.element__pic').src = card.link;
-  cardElement.querySelector('.element__like').addEventListener('click', function (evt) {
-    evt.target.classList.toggle('element__like_active');
-  });
-  cardContainer.append(cardElement);
-  return card;
+function composeItem(item) {
+  const newCardItem = cardTemplate.content.cloneNode(true);
+  const titleItem = newCardItem.querySelector('.element__title'); 
+  titleItem.textContent = item.name; 
+  const picItem = newCardItem.querySelector('.element__pic'); 
+  picItem.src = item.link; 
+  return newCardItem;
 }
 
+// popap / add card
+
+function addNewItem(evt){
+  evt.preventDefault();
+  const inputText = placeInput.value;
+  const inputLink = linkInput.value;
+  const newItemHTML = composeItem({ name: inputText, link: inputLink });
+  cardContainer.prepend(newItemHTML);
+  closePopup(evt);
+}
+
+// render + listeners
+
 renderCards();
+formEditProfile.addEventListener('submit', formSubmitHandler);
+formAddCard.addEventListener('submit', addNewItem);
 
