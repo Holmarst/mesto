3cb62208bdbeca1602912acf3e5cc8a038f6fff3
Liubov Kholmogorova <holmarst@gmail.com>
2020-12-22 14:39:33 +0300

    6 спринт: описана валидация полей

--- script/script.js
+++ script/script.js
@@ -162,3 +162,67 @@ function openPic(text, img){
 renderCards();
 formEditProfile.addEventListener('submit', editProfileInfo);
 formAddCard.addEventListener('submit', addNewItem);
+
+
+
+// validation
+
+function showError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = input.validationMessage;
+  input.classList.add('popup__input_type_invalid');
+  console.log('error');
+}
+
+function hideError(form, input) {
+  const error = form.querySelector(`#${input.id}-error`);
+  error.textContent = "";
+  input.classList.remove('popup__input_type_invalid');
+}
+
+function checkInputValidity(form, input) {
+  if (input.validity.valid) {
+    hideError(form, input);
+  } else {
+    showError(form, input);
+  }
+}
+
+function setButtonState (button, isActive) {
+  if (isActive) {
+    button.classList.remove('popup__button_activity_invalid');
+    button.disabled = false;
+  } else {
+    button.classList.add('popup__button_activity_invalid');
+    button.disabled = 'disabled';
+  }
+}
+
+function setEventListener(form) {
+  const inputList = form.querySelectorAll('.popup__input');
+  const submitButton = form.querySelector('.popup__button');
+
+  inputList.forEach( input => {
+    input.addEventListener('input', (evt) => {
+      checkInputValidity(form, input);
+      setButtonState(submitButton, form.checkValidity());
+    });
+  });
+}
+
+function enableValidation () {
+  const forms = document.querySelectorAll('.popup__container');
+
+  forms.forEach( form => {
+    setEventListener(form);
+
+    form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+
+    const submitButton = form.querySelector('.popup__button');
+    setButtonState(submitButton, form.checkValidity());
+  });
+}
+
+enableValidation();
